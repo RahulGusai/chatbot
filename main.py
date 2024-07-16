14d7714ea1999a92ac9cb653c27d3ba73aad1612
Rahul Gusai <rgusai97@gmail.com>
2024-07-16 14:29:37 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_anthropic import ChatAnthropic
+from langchain_core.runnables.history import RunnableWithMessageHistory
+import os
+import streamlit as st
+
+os.environ["ANTHROPIC_API_KEY"] = "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"
+
+model = ChatAnthropic(model="claude-3-sonnet-20240229")
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful assistant. Answer all questions to the best of your ability in {language}.",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+chain = prompt | model
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+if prompt := st.chat_input("What is up?"):
+    with st.chat_message("user"):
+        st.markdown(prompt)
+        st.session_state.messages.append(
+            {"role": "user", "content": prompt})
+
+    message_container = st.chat_message("assistant")
+    message_placeholder = message_container.empty()
+
+    # Stream the response
+    response_content = ""
+    for response in chain.stream({"messages": [HumanMessage(content=prompt)],
+                                  "language": "english"}):
+        response_content += response.content
+        message_placeholder.markdown(response_content)
+    st.session_state.messages.append(
+        {"role": "assistant", "content": response_content})
